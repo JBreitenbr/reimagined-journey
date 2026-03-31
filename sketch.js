432a3b01f43279c750f5ebae816df776e19e7d9d
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-03-31 19:05:11 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+let grid=20;
+
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(200,255,255);
-  circle(50, 50, 20);
+  background(0);
+  stroke(255);
+  noFill();
+  for(let x=0;x<width;x+=grid){
+    for(let y=0;y<height;y+=grid){
+      push();
+      translate(x,y);
+      drawRandomPattern();
+      pop();
+    }
+  }
+  noLoop();
+}
+
+function drawRandomPattern() {
+  let r=random(1);
+  if(r<0.5) line(0,grid,grid,0);
+  else line(0,0,grid,grid);
 }
