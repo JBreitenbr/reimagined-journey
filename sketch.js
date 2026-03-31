8a829cec02fa4ca30337c10fe3d7930329e56c68
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-03-31 16:46:57 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(300, 300);
 }
 
 function draw() {
-  background(200);
+  background(200,255,255);
   circle(50, 50, 20);
 }
